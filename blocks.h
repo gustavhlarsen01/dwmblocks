99c4916a36b574481c907e2079043383d8612c47
polkaprik <gustavhlarsen01@gmail.com>
2022-04-03 23:25:07 +0200

    Add files via upload

--- blocks.h
+++ blocks.h
@@ -1,11 +1,14 @@
 //Modify this file to change what commands output to your statusbar, and recompile using the make command.
 static const Block blocks[] = {
 	/*Icon*/	/*Command*/		/*Update Interval*/	/*Update Signal*/
-	{"", "free -h | awk '/^Mem/ { print $3\"/\"$2 }' | sed s/i//g", 	30,		0},
+	{"", "amixer get Master | awk '$0~/%/{print $4}' | tr -d '[]%'",		0,		10},
+
+	{"", "free -h | awk '/^Mem/ { print $3 }' | sed s/i//g", 	30,		0},
 
 	{"", "acpi | awk '{print $4}' | sed s/,//", 					5,		0},
 
-	{"", "date '+%T'", 					1,		0},
+	{"", "date '+%T'", 		1,		0},
+	
 };
 
 //sets delimeter between status commands. NULL character ('\0') means no delimeter.
